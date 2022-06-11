d671412a727d96502d020735224be5f196db8c36
Jozias Martini <jozias.martini@gmail.com>
2022-06-11 16:35:32 -0300

    Create basic functions, names, ages and numbers

--- index.js
+++ index.js
@@ -8,3 +8,35 @@ function randomFromArray(array) {
 function randomNumber(max) {
   return Math.floor(Math.random() * max);
 }
+
+// Functions
+export function personName(genre) {
+  if (genre) {
+    if (genre === "male") {
+      return randomFromArray(personMaleNames);
+    }
+    return randomFromArray(personFemaleNames);
+  } else {
+    if (Math.random() > 0.5) {
+      return randomFromArray(personMaleNames);
+    }
+    return randomFromArray(personFemaleNames);
+  }
+}
+
+export function personAge() {
+  return randomNumber(100);
+}
+
+export function petName() {
+  return randomFromArray(petNames);
+}
+
+export function phoneNumber() {
+  return randomFromArray(phoneNumbers);
+}
+
+console.log(personName());
+console.log(personAge());
+console.log(petName());
+console.log(phoneNumber());
