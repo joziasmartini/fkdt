c41cc56a3e84a315d48d858a7c470b365767820a
Jozias Martini <jozias.martini@gmail.com>
2022-06-11 16:34:27 -0300

    Create first helpers

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Helpers
+function randomFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function randomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
