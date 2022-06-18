7436f6abb14cd8626c4cc1acd15d704eda4067cc
Jozias Martini <jozias.martini@gmail.com>
2022-06-18 19:51:48 -0300

    Create UUID generator

--- src/index.js
+++ src/index.js
@@ -46,6 +46,19 @@ export function phone() {
   return mask;
 }
 
+function uuid() {
+  var dt = new Date().getTime();
+  var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+    /[xy]/g,
+    function (c) {
+      var r = (dt + Math.random() * 16) % 16 | 0;
+      dt = Math.floor(dt / 16);
+      return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+    }
+  );
+  return uuid;
+}
+
 export function age() {
   return randomNumber(100);
 }
@@ -63,3 +76,4 @@ console.log(age());
 console.log(pet());
 console.log(phone());
 console.log(job());
+console.log(uuid());
