ebf79f917b2b58b6f2a2fe612dec12e78990f6e3
Jozias Martini <jozias.martini@gmail.com>
2022-08-25 23:56:25 -0300

    Update functions on index file

--- src/index.js
+++ src/index.js
@@ -6,82 +6,52 @@ import {
   petNames,
   jobTitles,
   countryNames,
-  vowels,
-  consonants,
   emailDomains,
   stateNames,
   interests,
   colors,
 } from "./data.js";
 
-// Helpers
-function randomFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
-function randomNumber(max, min) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function randomStringWithNumbers(length) {
-  var result = "";
-  var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
-// Functions
-export function generatedName() {
-  let name = [
-    randomFromArray(vowels).toUpperCase(),
-    randomFromArray(consonants),
-    randomFromArray(vowels),
-    randomFromArray(consonants),
-    randomFromArray(vowels),
-  ];
-  return name.join("");
-}
+import {
+  randomElementFromArray,
+  randomNumber,
+  randomStringWithNumbers,
+} from "./helpers.js";
 
 export function name() {
   return Math.random() > 0.5
-    ? randomFromArray(personMaleNames)
-    : randomFromArray(personFemaleNames);
+    ? randomElementFromArray(personMaleNames)
+    : randomElementFromArray(personFemaleNames);
 }
 
 export function age() {
-  return randomNumber(100, 18);
+  return randomNumber(18, 100);
 }
 
 export function phone() {
   let number = "(xxx) xxx-xxxx";
 
   for (let i = 0; i < 10; i++) {
-    number = number.replace("x", randomNumber(10, 0));
+    number = number.replace("x", randomNumber(0, 10));
   }
 
   return number;
 }
 
 export function job() {
-  return randomFromArray(jobTitles);
+  return randomElementFromArray(jobTitles);
 }
 
 export function interest() {
-  return randomFromArray(interests);
+  return randomElementFromArray(interests);
 }
 
 export function color() {
-  return randomFromArray(colors);
+  return randomElementFromArray(colors);
 }
 
 export function pet() {
-  return randomFromArray(petNames);
+  return randomElementFromArray(petNames);
 }
 
 function uuid() {
@@ -104,7 +74,7 @@ export function password() {
 export function username() {
   let nameOne = name();
   let nameTwo = name();
-  let charNumber = randomNumber(5, 2);
+  let charNumber = randomNumber(2, 5);
   nameOne = nameOne.slice(0, charNumber);
   nameTwo = nameTwo.slice(0, charNumber);
   return `${nameOne}${nameTwo}`.toLowerCase();
@@ -121,29 +91,32 @@ export function ip() {
 }
 
 export function country() {
-  return randomFromArray(countryNames);
+  return randomElementFromArray(countryNames);
 }
 
 export function state() {
-  return randomFromArray(stateNames);
+  return randomElementFromArray(stateNames);
 }
 
 export function latitude() {
-  return `${randomNumber(99, 11)}.${randomNumber(9999, 1111)}`;
+  return `${randomNumber(11, 99)}.${randomNumber(1111, 9999)}`;
 }
 
 export function longitude() {
-  return `-${randomNumber(99, 11)}.${randomNumber(9999, 1111)}`;
+  return `-${randomNumber(11, 99)}.${randomNumber(1111, 9999)}`;
 }
 
 export function email() {
   let u = username();
-  let email = randomFromArray(emailDomains);
+  let email = randomElementFromArray(emailDomains);
   return `${u}@${email}`;
 }
 
 console.log(name());
 console.log(email());
+console.log(username());
+console.log(password());
+console.log(ip());
 console.log(age());
 console.log(phone());
 console.log(job());
@@ -151,9 +124,6 @@ console.log(interest());
 console.log(color());
 console.log(pet());
 console.log(uuid());
-console.log(password());
-console.log(username());
-console.log(ip());
 console.log(country());
 console.log(state());
 console.log(latitude());
